73defc20d07e8bd8f38c3d7ec3f5913cf1a2167b
d1str4ught <>
2026-02-09 02:25:35 +0100

    crash fix: when __ClearReserveDeleteWindowList() was destroying Windows, some might have pushed it's children for deletion to m_ReserveDeleteWindowList, invalidating iterators

--- src/EterPythonLib/PythonWindowManager.cpp
+++ src/EterPythonLib/PythonWindowManager.cpp
@@ -75,6 +75,7 @@ namespace UI
 	void CWindowManager::Destroy()
 	{
 		__ClearReserveDeleteWindowList();
+
 #ifdef __WINDOW_LEAK_CHECK__
 		std::set<CWindow*>::iterator i;
 		for (i=gs_kSet_pkWnd.begin(); i!=gs_kSet_pkWnd.end(); ++i)
@@ -430,7 +431,7 @@ namespace UI
 			pParentWin->DeleteChild(pWin);
 		}
 		pWin->Clear();
-		m_ReserveDeleteWindowList.push_back(pWin);
+		m_ReserveDeleteWindowList.insert(pWin);
 	}
 
 	BOOL CWindowManager::IsDragging()
@@ -710,16 +711,17 @@ namespace UI
 
 	void CWindowManager::__ClearReserveDeleteWindowList()
 	{
-		for (TWindowContainer::iterator itor = m_ReserveDeleteWindowList.begin(); itor != m_ReserveDeleteWindowList.end(); ++itor)
-		{
-			CWindow * pWin = *itor;
-#ifdef __WINDOW_LEAK_CHECK__
-			gs_kSet_pkWnd.erase(pWin);
-#endif
-			delete pWin;
-		}
-		m_ReserveDeleteWindowList.clear();
+		while (!m_ReserveDeleteWindowList.empty()) {
+			auto tmp = m_ReserveDeleteWindowList;
+			m_ReserveDeleteWindowList.clear();
 
+			for (CWindow* pWin : tmp) {
+#ifdef __WINDOW_LEAK_CHECK__
+				gs_kSet_pkWnd.erase(pWin);
+#endif
+				delete pWin;
+			}
+		}
 	}	
 
 	void CWindowManager::Update()
